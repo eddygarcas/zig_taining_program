dd3c125a27cff4f4452f6facebedba4a224a5018
duard <eduard@rzilient.club>
2025-09-26 18:03:59 +0200

    Multi-Thread log writing

--- 11 - Multithread log files/build.zig
+++ 11 - Multithread log files/build.zig
@@ -113,4 +113,10 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const test_artifact = b.addInstallArtifact(exe_unit_tests,
+        .{.dest_dir = .{.override = .{.custom = "tests"}}},
+    );
+    const install_test_step = b.step("install_test", "Create test binaries for debugging");
+    install_test_step.dependOn(&test_artifact.step);
 }
